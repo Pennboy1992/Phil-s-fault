60c52620b6b7f71cbdca199d6a2b1ee2c9588d7d
Phillip Valdez <Phillip0150@gmail.com>
2019-06-26 13:49:12 -0500

    Added tv.js to cli

--- cli.js
+++ cli.js
@@ -1,10 +1,11 @@
 //cli takes 2 inputs <show/actor> <name of>
-
+var tv = require("./tv.js");
 var command = process.argv[2];
-var name = process.argv[3];
+var name = process.argv.slice(3).join(" ");
 
 if(command === "show"){
-	//find show
+	theTv = new tv(name);
+	theTv.findShow();
 }
 else if(command === "actor"){
 	//find actor
