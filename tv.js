88f87d56551a4e103fccd100738509d096034fd4
Phillip Valdez <Phillip0150@gmail.com>
2019-06-26 13:53:29 -0500

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,42 +1,32 @@
 var axios = require("axios");
-function Tv(showName){
-    this.findShow = function(){
-        //make the API call using this.url
-      this.url =  "http://api.tvmaze.com/singlesearch/shows?q=" + showName;
-      
-      // Run the axios.get function...
-      // The axios.get function takes in a URL and returns a promise (just like $.ajax)
-      axios
-        .get(this.url)
-        .then(function(response) {
-          // If the axios was successful...
-          // Then log the body from the site!
-          console.log(response.data.name);
-          for(x in response.data.genres){
-          console.log(response.data.genres[x]);
-          }
-          console.log(response.data.rating.average);
-          console.log(response.data.network.name);
-          console.log(response.data.summary);
-        })
-        .catch(function(error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });
-    }
+var fs = require("fs");
+
+function output(info){
+  console.log(info);
+
+  fs.appendFile("log.txt", info, function(err){
+    console.log(err);
+  })
+}
+
+var TV = function(){
+  this.findShow = function(show){
+    var URL = "https://api.tvmaze.com/singlesearch/shows?q=" + show
+
+    axios.get(URL).then(function(response){
+      //console.log(response);
+      output(`
+      Show Name: ${response.data.name}
+      Genres: ${response.data.genres.join(" ")}
+      Network: ${response.data.network.name}
+      Summary: ${response.data.summary}
+      -----------------------------------
+      `);
+
+    })
   }
-  
-  module.exports = Tv;
+  this.findActor = function(){
+  }
+}
+
+module.exports = TV;
